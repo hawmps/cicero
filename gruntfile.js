f2ca82fcea528a0c174c8658c945138fce9e246e
hawmps <jp@lumsdaine.holdings>
2014-03-08 16:08:42 +1100

    JSHint added to grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,29 @@
 //Gruntfile
 module.exports = function(grunt) {
+	//'use strict'
 	grunt.initConfig({
     	//tasks go here
+    	//Metadata
+    	pkg: grunt.file.readJSON('package.json'),
+    	banner: '/*!\n' +
+            ' * Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' */\n',
+        jshint: {
+  			// define the files to lint
+	  		files: ['gruntfile.js', 'app.js'],
+	  		// configure JSHint (documented at http://www.jshint.com/docs/)
+	  		options: {
+	      	// more options here if you want to override JSHint defaults
+	    		globals: {
+	      			jQuery: true,
+	      			console: true,
+	      			module: true
+	    		}
+	  		}
+		}	    
+    })
+	grunt.loadNpmTasks('grunt-contrib-jshint')
+	grunt.registerTask('default', ['jshint'])
 }
